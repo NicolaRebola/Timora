5e31f39d59a5ce10ce9cdd22ce383868dd6b6070
Nicola Rebola <nicola.rebola@webee.io>
2024-12-03 16:16:13 -0300

    Initial Commit

---
+++ src/main/java/com/nicola_rebola/timora/TimoraApplication.java
@@ -0,0 +1,13 @@
+package com.nicola_rebola.timora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicola_rebola/timora/TimoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicola_rebola.timora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
